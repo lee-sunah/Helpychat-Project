d50bb57e763be5610ca420e756452f956243a0fc
strongrry <kanghaelee@naver.com>
2025-11-18 11:51:34 +0900

    수정

--- project_root/src/utils/allure_helper.py
+++ project_root/src/utils/allure_helper.py
@@ -4,7 +4,7 @@ from datetime import datetime
 # 스크린샷 찍는 코드
 def attach_screenshot(driver, name="screenshot"):
     allure.attach(
-        driver.get_screenshot_aspng(),
+        driver.get_screenshot_as_png(),
         name=f"{name}{datetime.now().strftime('%H-%M-%S')}",
         attachment_type=allure.attachment_type.PNG
     )
